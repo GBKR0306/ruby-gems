8876fe157b20f606f4d716a36ba18a4580f9fd9c
GBKR0306 <gbkr0306@gmail.com>
2023-05-11 17:25:07 +0000

    Ban Unban

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def ban
+    @user = User.find(params[:id])
+    if @user.access_locked?
+      @user.unlock_access!
+    else
+    @user.lock_access!
+    end
+    redirect_to @user, notice: "User ban status updated"
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User was successfully deleted"
+  end
+end
